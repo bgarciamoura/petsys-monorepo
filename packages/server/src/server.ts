3690ff4b8cae28bd4a0fbee8c7bdf40b338363e9
Bruno Garcia Moura <bgarciamoura@gmail.com>
2022-04-09 20:05:18 -0300

    feat: Initialized the prima client and created the firsts tables

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,14 +1,19 @@
 import express from 'express';
 import cors from 'cors';
+import * as dotenv from 'dotenv';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+dotenv.config();
+
 const PORT = process.env.SERVER_PORT || 3000;
 
+console.log(process.env.SERVER_PORT);
+
 app.get('/', (req, res) => {
-    res.json("Hello World, this is my first monorepo and i'm already using docker!");
+    res.json("Hello World, this is my first monorepo and i'm already using docker, i'm using prisma too!");
 });
 
 export { app, PORT };
